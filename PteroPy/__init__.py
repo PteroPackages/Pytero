79650dc186846cebf33bdda4368ba0369a63f3f0
devnote-dev <pokeraven4@gmail.com>
2021-11-06 18:10:46 +0000

    feat(pkg): package annotations

--- PteroPy/__init__.py
+++ PteroPy/__init__.py
@@ -1,3 +1,16 @@
+'''# PteroPy
+### An updated and flexible API wrapper for the Pterodactyl API!
+
+Author: Devonte <https://github.com/devnote-dev>
+© 2021 devnote-dev
+License: MIT
+'''
+
+__title__ = 'pteropy'
+__author__ = 'Devonte'
+__copyright__ = 'MIT'
+__version__ = '0.0.1a'
+
 from .app.pteroapp import *
 from .app.app_requests import *
 from .app.app_servers import *

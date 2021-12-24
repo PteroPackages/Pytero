a605cc93731ddd5fd74a2ac1e97491a1f3fc3ab6
devnote-dev <pokeraven4@gmail.com>
2021-12-24 02:33:04 +0000

    feat(app): formatting & prop fixes

--- PteroPy/app/app.py
+++ PteroPy/app/app.py
@@ -8,31 +8,27 @@ class PteroApp:
     def __init__(self, domain: str, auth: str, **kwargs) -> None:
         self.domain = domain.removesuffix('/')
         self.auth = auth
-        self.options: Dict[str, Any] = None
-        
-        self.ready_at: float = None
-        self.ping: float = None
-        
-        self.requests = RequestManager(self, 'application')
+        self.options: Dict[str, Any] = {}
+        self.ready_at: float = 0.0
+        self.ping: float = -1.0
+        self.requests: RequestManager = None
         self.servers = ApplicationServerManager(self)
     
     def __repr__(self) -> str:
-        return '<PteroApp %ims>' % self.ping or -1
+        return '<PteroApp %ims>' % self.ping
     
     async def connect(self) -> bool:
-        if self.ready_at is not None:
-            raise Exception('pteroapp already connected')
+        if self.requests is None:
+            self.requests = RequestManager(self, 'application')
         
-        start = time()
-        self.requests.ping()
-        self.ping = time() - start
+        await self.requests.ping()
         self.ready_at = time()
         return True
     
     async def close(self) -> None:
-        if self.ready_at is None:
+        if self.requests.session is None:
             return
         
-        await self.requests.session.close()
-        self.ping = None
-        self.ready_at = None
+        await self.requests.close()
+        self.ping = -1.0
+        self.ready_at = 0.0

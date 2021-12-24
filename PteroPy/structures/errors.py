1387e52c4e5a90c9a5110adc6acf2307ccf99fcd
devnote-dev <pokeraven4@gmail.com>
2021-12-24 02:31:05 +0000

    fix(errors): fix token for format str

--- PteroPy/structures/errors.py
+++ PteroPy/structures/errors.py
@@ -5,7 +5,7 @@ class RequestError(Exception):
 class PteroAPIError(Exception):
     def __init__(self, data: object) -> None:
         err = data['errors'][0]
-        super().__init__('%d: %s' % (err['status'], err['detail']))
+        super().__init__('%s: %s' % (err['status'], err['detail']))
 
 
 class WebSocketError(Exception):

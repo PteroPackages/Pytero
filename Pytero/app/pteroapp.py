216ac8b20195cba911ad42daaf70b0e5229edece
devnote-dev <inferno0745@gmail.com>
2022-02-18 00:25:36 +0000

    fix(app): clean code & fix connect

--- Pytero/app/pteroapp.py
+++ Pytero/app/pteroapp.py
@@ -1,19 +1,31 @@
-from .apprequestmanager import AppRequestManager
+from time import time
+from ..requests import RequestError, PteroAPIError, RequestManager
 
 
 class PteroApp:
     def __init__(self, domain: str, auth: str, **kwargs):
         self.domain = domain.removesuffix('/')
         self.auth = auth
-        self.options = None
+        self.options = None # TODO: use kwargs
         
-        self.ready_at: int = None
-        self.ping: float = None
+        self.ready_at = 0.0
+        self.ping = -1.0
         
-        self.requests = AppRequestManager(self)
+        self.requests = RequestManager('Application', self.domain, self.auth)
     
-    async def connect(self):
+    async def connect(self) -> bool:
         if self.ready_at:
             raise Exception('pteroapp already connected')
         
-        return NotImplemented
+        start = time()
+        try:
+            await self.requests.rget('/application/api')
+        except PteroAPIError:
+            self.ping = time() - start
+        except Exception as e:
+            raise RequestError(
+                'pterodactyl api is unavailable\nresponse: %s' % str(e)
+            )
+        
+        self.ready_at = time()
+        return True

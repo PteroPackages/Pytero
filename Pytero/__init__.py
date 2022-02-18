2085e626f8115f4a8338c42d09818b39005b2fa5
devnote-dev <inferno0745@gmail.com>
2022-02-18 00:28:57 +0000

    feat: update package

---
+++ Pytero/__init__.py
@@ -0,0 +1,16 @@
+'''# Pytero
+An updated and flexible API wrapper for the Pterodactyl API!
+
+Author: Devonte <https://github.com/devnote-dev>
+Repository: https://github.com/PteroPackages/Pytero
+License: MIT
+
+© 2021-2022 PteroPackages
+'''
+from .app.pteroapp import PteroApp
+from .requests import RequestManager
+from .errors import *
+
+__title__ = 'Pytero'
+__version__ = '0.0.1a'
+__license__ = 'MIT'

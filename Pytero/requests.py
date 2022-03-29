cb6835ff6becf06eb9e3f4371c9b1d8c57c18be0
devnote-dev <inferno0745@gmail.com>
2022-03-29 22:35:25 +0100

    feat(requests): exclude invalid params from body

--- Pytero/requests.py
+++ Pytero/requests.py
@@ -68,9 +68,14 @@ class RequestManager(EventManager):
             if p := params.get('raw'):
                 await self.__debug('sending raw byte payload')
                 body = p
-            elif p := params.get('body'):
-                await self.__debug('sending json payload')
-                body = dumps(p)
+            else:
+                copy = dict(params)
+                for key in ('page', 'per_page', 'filter', 'include', 'sort'):
+                    copy.pop(key, None)
+                
+                if len(copy):
+                    await self.__debug('sending json payload')
+                    body = dumps(copy)
         
         query = self._validate_query(params)
         url = '%s/api/%s%s%s' % (self.domain, self._type.lower(), path, query)

11f489711979348128f2cfb6c6ace2a8abd6d091
devnote-dev <inferno0745@gmail.com>
2022-07-04 21:40:34 +0100

    refactor: redo permissions & flags

--- Pytero/permissions.py
+++ Pytero/permissions.py
@@ -2,156 +2,167 @@ from enum import Enum
 
 
 class Flags(Enum):
-    WEBSOCKET_CONNECT = 0
+    WEBSOCKET_CONNECT = 'websocket.connect'
 
-    CONTROL_CONSOLE = 1
-    CONTROL_START = 2
-    CONTROL_STOP = 3
-    CONTROL_RESTART = 4
+    CONTROL_CONSOLE = 'control.console'
+    CONTROL_START = 'control.start'
+    CONTROL_STOP = 'control.stop'
+    CONTROL_RESTART = 'control.restart'
 
-    USER_CREATE = 5
-    USER_READ = 6
-    USER_UPDATE = 7
-    USER_DELETE = 8
+    USER_CREATE = 'user.create'
+    USER_READ = 'user.read'
+    USER_UPDATE = 'user.update'
+    USER_DELETE = 'user.delete'
 
-    FILE_CREATE = 9
-    FILE_READ = 10
-    FILE_UPDATE = 11
-    FILE_DELETE = 12
-    FILE_ARCHIVE = 13
-    FILE_SFTP = 14
+    FILE_CREATE = 'file.create'
+    FILE_READ = 'file.read'
+    FILE_READ_CONTENT = 'file.read-content'
+    FILE_UPDATE = 'file.update'
+    FILE_DELETE = 'file.delete'
+    FILE_ARCHIVE = 'file.archive'
+    FILE_SFTP = 'file.sftp'
 
-    BACKUP_CREATE = 15
-    BACKUP_READ = 16
-    BACKUP_UPDATE = 17
-    BACKUP_DELETE = 18
+    BACKUP_CREATE = 'backup.create'
+    BACKUP_READ = 'backup.read'
+    BACKUP_UPDATE = 'backup.update'
+    BACKUP_DELETE = 'backup.delete'
 
-    ALLOCATION_READ = 19
-    ALLOCATION_CREATE = 20
-    ALLOCATION_UPDATE = 21
-    ALLOCATION_DELETE = 22
+    ALLOCATION_READ = 'allocation.read'
+    ALLOCATION_CREATE = 'allocation.create'
+    ALLOCATION_UPDATE = 'allocation.update'
+    ALLOCATION_DELETE = 'allocation.delete'
 
-    STARTUP_READ = 23
-    STARTUP_UPDATE = 24
+    STARTUP_READ = 'startup.read'
+    STARTUP_UPDATE = 'sartup.update'
 
-    DATABASE_CREATE = 25
-    DATABASE_READ = 26
-    DATABASE_UPDATE = 27
-    DATABASE_DELETE = 28
-    DATABASE_VIEW_PASSWORD = 29
+    DATABASE_CREATE = 'database.create'
+    DATABASE_READ = 'database.read'
+    DATABASE_UPDATE = 'database.update'
+    DATABASE_DELETE = 'database.delete'
+    DATABASE_VIEW_PASSWORD = 'database.view_password'
 
-    SCHEDULE_CREATE = 30
-    SCHEDULE_READ = 31
-    SCHEDULE_UPDATE = 32
-    SCHEDULE_DELETE = 33
+    SCHEDULE_CREATE = 'schedule.create'
+    SCHEDULE_READ = 'schedule.read'
+    SCHEDULE_UPDATE = 'schedule.update'
+    SCHEDULE_DELETE = 'schedule.delete'
 
-    SETTINGS_RENAME = 34
-    SETTINGS_REINSTALL = 35
+    SETTINGS_RENAME = 'settings.rename'
+    SETTINGS_REINSTALL = 'settings.reinstall'
 
-    ADMIN_WEBSOCKET_ERRORS = 41
-    ADMIN_WEBSOCKET_INSTALL = 42
-    ADMIN_WEBSOCKET_TRANSFER = 43
+    ADMIN_WEBSOCKET_ERRORS = 'admin.websocket.errors'
+    ADMIN_WEBSOCKET_INSTALL = 'admin.websocket.install'
+    ADMIN_WEBSOCKET_TRANSFER = 'admin.websocket.transfer'
+
+    def __contains__(self, key: str) -> bool:
+        return super().__contains__(key)
 
     @staticmethod
-    def get_string_keys() -> list[str]:
-        return list(Flags.__members__.keys())
-    
-    @staticmethod
-    def get_key_values() -> list[int]:
-        return [k.value for k in Flags.__members__.values()]
-
-
-def diff(perms: list) -> bool:
-    base = type(perms[0])
-    for p in perms:
-        if not isinstance(p, base):
-            return True
-    
-    return False
+    def values(cls) -> list[str]:
+        return [p.value for p in Flags.__members__.values()]
 
 
 class Permissions:
-    ALL_CONSOLE = (1, 2, 3, 4)
-    ALL_USER = (5, 6, 7, 8)
-    ALL_FILE = (9, 10, 11, 12, 13, 14)
-    ALL_BACKUP = (15, 16, 17, 18)
-    ALL_ALLOCATION = (19, 20, 21, 22)
-    ALL_STARTUP = (23, 24)
-    ALL_DATABASE = (25, 26, 27, 28, 29)
-    ALL_SCHEDULE = (30, 31, 32, 33)
-    ALL_SETTINGS = (34, 35)
-    ALL_ADMIN = (41, 42, 43)
+    ALL_CONSOLE = (
+        Flags.CONTROL_CONSOLE,
+        Flags.CONTROL_START,
+        Flags.CONTROL_STOP,
+        Flags.CONTROL_RESTART
+    )
+    ALL_USER = (
+        Flags.USER_CREATE,
+        Flags.USER_READ,
+        Flags.USER_UPDATE,
+        Flags.USER_DELETE
+    )
+    ALL_FILE = (
+        Flags.FILE_CREATE,
+        Flags.FILE_READ,
+        Flags.FILE_READ_CONTENT,
+        Flags.FILE_UPDATE,
+        Flags.FILE_ARCHIVE,
+        Flags.FILE_SFTP
+    )
+    ALL_BACKUP = (
+        Flags.BACKUP_CREATE,
+        Flags.BACKUP_READ,
+        Flags.BACKUP_UPDATE,
+        Flags.BACKUP_DELETE
+    )
+    ALL_ALLOCATION = (
+        Flags.ALLOCATION_CREATE,
+        Flags.ALLOCATION_READ,
+        Flags.ALLOCATION_UPDATE,
+        Flags.ALLOCATION_DELETE
+    )
+    ALL_STARTUP = (Flags.STARTUP_READ, Flags.STARTUP_UPDATE)
+    ALL_DATABASE = (
+        Flags.DATABASE_CREATE,
+        Flags.DATABASE_READ,
+        Flags.DATABASE_UPDATE,
+        Flags.DATABASE_DELETE,
+        Flags.DATABASE_VIEW_PASSWORD
+    )
+    ALL_SCHEDULE = (
+        Flags.SCHEDULE_CREATE,
+        Flags.SCHEDULE_READ,
+        Flags.SCHEDULE_UPDATE,
+        Flags.SCHEDULE_DELETE
+    )
+    ALL_SETTINGS = (Flags.SETTINGS_RENAME, Flags.SETTINGS_REINSTALL)
+    ALL_ADMIN = (
+        Flags.ADMIN_WEBSOCKET_ERRORS,
+        Flags.ADMIN_WEBSOCKET_INSTALL,
+        Flags.ADMIN_WEBSOCKET_TRANSFER
+    )
     
-    def __init__(self, data: dict[str, int] | list[str] | list[int]) -> None:
-        self.raw = self.resolve(data)
-    
-    def __repr__(self) -> str:
-        return '<Permissions total=%d>' % len(self.raw)
-    
-    def __len__(self) -> int:
-        return len(self.raw)
-    
-    def __contains__(self, perms):
-        return self.has(perms)
+    def __init__(self, *perms: str | Flags) -> None:
+        self.value: list[str] = self.resolve(*perms)
     
     @staticmethod
-    def ALL() -> tuple[str]:
-        res = list(range(0, 36)) + [41, 42, 43]
-        return tuple(res)
+    def resolve(*perms: str | Flags) -> list[str]:
+        res: list[str] = []
+        
+        for perm in perms:
+            match type(perm).__name__:
+                case 'Flags':
+                    res.append(perm.value)
+                case 'str':
+                    if perm in Flags:
+                        res.append(perm)
+                    else:
+                        raise KeyError('Invalid permission or flag')
+                case _:
+                    raise KeyError('Invalid permission or flag')
+        
+        return res
     
-    def has(self, perms: dict[str, int] | list[str] | list[int]) -> bool:
-        res = self.resolve(perms)
-        return res in self.raw
+    def __repr__(self) -> str:
+        return '<Permissions total=%d>' % len(self.value)
     
-    def is_admin(self) -> bool:
-        for f in self.to_list():
-            if 'ADMIN' in f:
+    def __len__(self) -> int:
+        return len(self.value)
+    
+    def __contains__(self, perm: str):
+        return perm in self.value
+    
+    def any(self, *perms) -> bool:
+        res = self.__class__.resolve(*perms)
+        for perm in self.value:
+            if perm in res:
                 return True
         
         return False
     
-    @staticmethod
-    def resolve(perms: object) -> dict[str, int]:
-        if isinstance(perms, dict):
-            perms = [k for k in perms.keys()]
+    def all(self, *perms) -> bool:
+        res = self.__class__.resolve(*perms)
+        for perm in self.value:
+            if perm not in res:
+                return False
         
-        if len(perms) == 0:
-            return {}
-        
-        if diff(perms):
-            raise TypeError('permissions must be all strings or all ints')
-        
-        keys = dict(zip(Flags.get_string_keys(), Flags.get_key_values()))
-        vals = {keys[k]: k for k in keys}
-        res = {}
-        for p in perms:
-            if keys.get(p):
-                res[p] = keys[p]
-            elif vals.get(p):
-                res[vals[p]] = p
-            else:
-                raise KeyError(f"unknown permission '{p}'")
-        
-        return res
+        return True
+    
+    def is_admin(self) -> bool:
+        return any(filter(lambda p: 'admin' in p, self.value))
     
     def serialize(self) -> dict[str, bool]:
-        return {k: k in self.raw for k in Flags.get_string_keys()}
-    
-    def to_list(self) -> list[str]:
-        return [k for k in self.raw]
-    
-    def to_strings(self) -> list[str]:
-        return [f.lower().replace('_', '.') for f in self.to_list()]
-    
-    @staticmethod
-    def from_strings(perms: list[str]) -> dict[str, int]:
-        keys = dict(zip(Flags.get_string_keys(), Flags.get_key_values()))
-        res = {}
-        for p in perms:
-            p = p.upper().replace('.', '_')
-            if keys.get(p):
-                res[p] = keys[p]
-            else:
-                raise KeyError(f"unknown permission '{p}'")
-        
-        return res
+        return {k: k in self.value for k in Flags.values()}

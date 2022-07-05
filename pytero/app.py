1c7e39ba2235219d4bdd256a9f774e2f85f13859
devnote-dev <inferno0745@gmail.com>
2022-07-05 11:20:25 +0100

    feat: implement server methods

--- pytero/app.py
+++ pytero/app.py
@@ -1,4 +1,6 @@
 from .http import RequestManager
+from .servers import AppServer
+from .types import DeployOptions, FeatureLimits, Limits
 from .users import User
 
 
@@ -11,7 +13,7 @@ class PteroApp:
         self._http = RequestManager('application', self.url, key)
     
     def __repr__(self) -> str:
-        return 'PteroApp'
+        return '<PteroApp>'
     
     @property
     def event(self):
@@ -21,8 +23,8 @@ class PteroApp:
         data = await self._http.get('/users')
         res: list[User] = []
         
-        for attr in data['data']:
-            res.append(User(self._http, attr['attributes']))
+        for datum in data['data']:
+            res.append(User(self._http, datum['attributes']))
         
         return res
     
@@ -86,3 +88,138 @@ class PteroApp:
     
     async def delete_user(self, id: int) -> None:
         await self._http.delete(f'/users/{id}')
+    
+    async def get_servers(self) -> list[AppServer]:
+        data = await self._http.get('/servers')
+        res: list[AppServer] = []
+        
+        for datum in data['data']:
+            res.append(AppServer(self._http, datum['attributes']))
+        
+        return res
+    
+    async def get_server(self, id: int | str) -> AppServer:
+        path = f'/users/{id}' if isinstance(id, int) else f'/users/external/{id}'
+        data = await self._http.get(path)
+        return AppServer(self._http, data['attributes'])
+    
+    async def create_server(
+        self,
+        *,
+        name: str,
+        user: int,
+        egg: int,
+        docker_image: str,
+        startup: str,
+        environment: dict[str, int | str | bool],
+        limits: Limits,
+        feature_limits: FeatureLimits,
+        external_id: str = None,
+        default_allocation: int = None,
+        additional_allocations: list[int] = None,
+        deploy: DeployOptions = None,
+        skip_scripts: bool = False,
+        oom_disabled: bool = True,
+        start_on_completion: bool = False
+    ) -> AppServer:
+        body = {
+            'name': name,
+            'user': user,
+            'egg': egg,
+            'docker_image': docker_image,
+            'startup': startup,
+            'environment': environment,
+            'limits': dict(limits),
+            'feature_limits': dict(feature_limits),
+            'external_id': external_id,
+            'skip_scripts': skip_scripts,
+            'oom_disabled': oom_disabled,
+            'start_on_completion': start_on_completion}
+        
+        if deploy is not None:
+            body['deploy'] = dict(deploy)
+        else:
+            body['allocation'] = {
+                'default': default_allocation,
+                'additional': additional_allocations}
+        
+        data = await self._http.post('/servers', body=body)
+        return AppServer(self._http, data['attributes'])
+    
+    async def update_server_details(
+        self,
+        id: int,
+        *,
+        external_id: str = None,
+        name: str = None,
+        user: int = None,
+        description: str = None
+    ) -> AppServer:
+        old = await self.get_server(id)
+        data = await self._http.patch(
+            f'/servers/{id}/details',
+            body={
+                'external_id': external_id or old.external_id,
+                'name': name or old.name,
+                'user': user or old.user,
+                'description': description or old.description}
+        )
+        return AppServer(self._http, data['attributes'])
+    
+    async def update_server_build(
+        self,
+        id: int,
+        *,
+        allocation: int = None,
+        oom_disabled: bool = True,
+        limits: Limits = None,
+        feature_limits: FeatureLimits = None,
+        add_allocations: list[int] = [],
+        remove_allocations: list[int] = []
+    ) -> AppServer:
+        old = await self.get_server(id)
+        data = await self._http.patch(
+            f'/servers/{id}/build',
+            body={
+                'allocation': allocation or old.allocation_id,
+                'oom_disabled': oom_disabled,
+                'limits': dict(limits if limits is not None else old.limits),
+                'feature_limits': dict(feature_limits if feature_limits is not None else old.feature_limts),
+                'add_allocations': add_allocations,
+                'remove_allocations': remove_allocations}
+        )
+        return AppServer(self._http, data['attributes'])
+    
+    async def update_server_startup(
+        self,
+        id: int,
+        *,
+        startup: str = None,
+        environment: dict[str, int | str | bool] = None,
+        egg: int = None,
+        image: str = None,
+        skip_scripts: bool = False
+    ) -> AppServer:
+        old = await self.get_server(id)
+        data = await self._http.patch(
+            f'/servers/{id}/startup',
+            body={
+                'startup': startup or old.container['startup_command'],
+                'environment': environment or old.container['environment'],
+                'egg': egg or old.container['egg'],
+                'image': image or old.container['docker_image'],
+                'skip_scripts': skip_scripts}
+        )
+        return AppServer(self._http, data['attributes'])
+    
+    async def suspend_server(self, id: int, /) -> None:
+        await self._http.post(f'/servers/{id}/suspend')
+    
+    async def unsuspend_server(self, id: int, /) -> None:
+        await self._http.post(f'/servers/{id}/unsuspend')
+    
+    async def reinstall_server(self, id: int, /) -> None:
+        await self._http.post(f'/servers/{id}/reinstall')
+    
+    async def delete_server(self, id: int, *, force: bool = False) -> None:
+        await self._http.delete('/servers/%d%s' % (id, '/force' if force else ''))
